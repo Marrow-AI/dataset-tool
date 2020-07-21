2744954a06a0d05c9f61da2d924163acc35ff6db
shirin <shirinanlen@gmail.com>
2020-07-21 11:21:11 -0400

    fixing first page btn

--- client/data-tool/src/components/Search.js
+++ client/data-tool/src/components/Search.js
@@ -2,22 +2,17 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector } from 'react-redux'
 import store from '../state'
-import { useHistory, withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Header from './Header.js'
 
-const Search = (props) => {
+export default function Search() {
   const { register, handleSubmit } = useForm({ mode: "onBlur" });
-  const [btn, setBtn] = useState(true);
   const [datasetSession, setDatasetSession] = useState(true);
-
-  const socket = useSelector(state => state.socket)
   const socketSessionId = useSelector(state => state.socket ? state.socket.id : 0)
   let history = useHistory();
 
-  function handleSession(e) {
-    e.preventDefault()
-    console.log("Create session with socket id", socketSessionId);
-    fetch('http://localhost:8080/session', {
+  async function onSubmit(formData) {
+    await fetch('http://localhost:8080/session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ keyword: 'cats', socket: socketSessionId })
@@ -27,47 +22,41 @@ const Search = (props) => {
         console.log(data);
         setDatasetSession(data['dataset_session']);
         if (data.result === "OK") {
+          fetch('http://localhost:8080/search', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ keyword: 'cats', session: datasetSession, socket: socketSessionId })
+          })
+            .then(res => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data.result === "OK") {
+              } else {
+                alert(data.result);
+              }
+            })
         } else {
           alert(data.result);
         }
       })
-  }
-
-  const onSubmit = (formData) => {
-    console.log("Search");
-  
-    fetch('http://localhost:8080/search', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ keyword: 'cats', session: datasetSession, socket: socketSessionId })
+    console.log("Results are coming!")
+    store.dispatch({
+      type: 'SAVE_KEYWORD',
+      keyword: formData.keyword
     })
-      .then(res => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.result === "OK") {
-        } else {
-          alert(data.result);
-        }
-      })
-      store.dispatch({
-        type: 'SAVE_KEYWORD', 
-        keyword: formData.keyword
-      })
     history.push("/edit");
   }
 
   return (
     <>
-    <Header />
-    <div className='input'>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="keyword" autoComplete="off" placeholder="type a word..." ref={register({ required: true })} />
-        <button name="session" value={btn} className="create-session" onClick={handleSession} ref={register}>Create session</button>
-        <button name="search" type="submit" value={btn} className="search" ref={register}> Search </button>
-      </form>
-    </div>
+      <Header />
+      <div className='input'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input name="keyword" autoComplete="off" placeholder="type a word..." ref={register({ required: true })} />
+          <button name="search" type="submit" className="search" ref={register}> Search </button>
+        </form>
+      </div>
     </>
   )
 }
 
-export default withRouter (Search);
