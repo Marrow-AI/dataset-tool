59ba4afe94812e4e54cfe9729e42a640329d2a9a
Avnerus <avnerus@gmail.com>
2020-08-12 22:19:26 +0300

    Working on augmentation

--- densepose/server.py
+++ densepose/server.py
@@ -37,7 +37,8 @@ from detectron.utils.timer import Timer
 import detectron.core.test_engine as infer_engine
 import detectron.datasets.dummy_datasets as dummy_datasets
 import detectron.utils.c2 as c2_utils
-from vis_utils import vis_one_image
+
+from itertools import permutations
 
 c2_utils.import_detectron_ops()
 
@@ -88,6 +89,115 @@ def stringToImage(base64_string):
 def toRGB(image):
   return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
 
+def process_image(input_image):
+  timers = defaultdict(Timer)
+  t = time.time()
+  image = stringToImage(input_image[input_image.find(",")+1:])
+  img = toRGB(image)
+  size = img.shape[:2]
+  with c2_utils.NamedCudaScope(0):
+    cls_boxes, cls_segms, cls_keyps, cls_bodys = infer_engine.im_detect_all(
+      model, img, None, timers=timers
+    )
+
+  t2 = time.time()
+
+  #logger.info('Inference time: {:.3f}s'.format(t2 - t))
+  if cls_bodys is not None:
+    (finals,num_people) = process_bodies(img, cls_boxes, cls_bodys)
+    logger.info('Num of people {}'.format(num_people))
+    if len(finals) > 0:
+        retval, buffer = cv2.imencode('.jpg', finals[0])
+        jpg_as_text = base64.b64encode(buffer)
+        return jpg_as_text
+    else:
+      print('Skipping')
+      return None
+  else:
+    print('Skipping')
+    return None
+
+def process_bodies(im, boxes, body_uv):
+    if isinstance(boxes, list):
+        box_list = [b for b in boxes if len(b) > 0]
+        if len(box_list) > 0:
+            boxes = np.concatenate(box_list)
+        else:
+            boxes = None
+
+    IUV_fields = body_uv[1]
+    #print("Shape of image {}".format(im.shape))
+    K = 26
+
+    inds = np.argsort(boxes[:,0])
+    good_inds = inds[boxes[inds[:],4] >= 0.95]
+    #print("Indexes {}".format(good_inds))
+
+
+    good_size = np.full(good_inds.shape, True, dtype=bool)
+    for i, ind in enumerate(good_inds):
+        shape = IUV_fields[ind].shape
+        #print("Shape {}".format(shape))
+        good_size[i] = shape[2] >= 115
+
+    #print("Shapes filter {}".format(good_size))
+    real_good_inds = good_inds[good_size]
+    num_people = len(real_good_inds)
+    #print("Indexes {}".format(good_inds))
+        
+    print('Found {} people'.format(len(good_inds)))
+    finals = []
+    if len(real_good_inds) >= 1:
+        perms = list(set(permutations(real_good_inds, len(real_good_inds))))
+        np.random.shuffle(perms)
+        for i in range(4):
+            good_inds = perms[i]
+            print("Indexes {}".format(good_inds))
+
+    #print("Chose random 4 {}".format(good_inds))
+
+            Final = np.zeros([512,512, 3])
+            current_offset = 0
+
+            #np.random.shuffle(good_inds)
+            #print("Good index {}".format(good_inds))
+
+            for i, ind in enumerate(good_inds):
+                entry = boxes[ind,:]
+                #print("Box position: {}, score: {}".format(entry[0],entry[4]))
+                entry=entry[0:4].astype(int)
+                ####
+                output = IUV_fields[ind]
+                ####
+                ###
+                CurrentMask = (output[0,:,:]>0).astype(np.float32)
+                BlackMask = (output[0,:,:]==0)
+                #print("Shape of mask {}".format(CurrentMask.shape))
+                #print("Shape of output {}".format(output.shape))
+                #All_Coords = np.zeros([output.shape[1],output.shape[2], 3])
+                #All_Coords_Old = All_Coords[:output.shape[1],:output.shape[2],:]
+                #All_Coords_Old[All_Coords_Old==0]= im[entry[1]:output.shape[1]+entry[1],entry[0]:output.shape[2]+entry[0],:][All_Coords_Old==0]
+                #All_Coords[ 0 : output.shape[1], 0 : output.shape[2],:]= All_Coords_Old
+
+                All_Coords = np.array(im[entry[1]:output.shape[1]+entry[1],entry[0]:output.shape[2]+entry[0],:])
+                #cv2.imwrite(os.path.join(output_dir, 'coords{}.png'.format(ind)), All_Coords)
+                #cv2.imwrite(os.path.join(output_dir, 'img{}.png'.format(ind)), im)
+                All_Coords[BlackMask] = 0
+                All_Coords = All_Coords.astype(np.uint8)
+                resize_ratio = 128 / output.shape[2]
+                #print("Resize ratio {} ({})".format(resize_ratio, output.shape[2]))
+                img = cv2.resize(All_Coords, (128,int(output.shape[1] * resize_ratio)))
+
+                #cv2.imwrite(os.path.join(output_dir, 'person{}.png'.format(ind)), img)
+
+                Final[128:img.shape[0]+128,current_offset:img.shape[1] + current_offset,:] = img[:384,:,:]
+
+                current_offset = current_offset + 128
+
+            finals.append(Final)
+
+    return (finals, num_people)
+
 # Main
 def main(input_img, with_pix2pix = False):
   image = stringToImage(input_img[input_img.find(",")+1:])
@@ -132,7 +242,7 @@ def infer():
 def pose():
   print("Running pose!!")
   content = request.get_json(silent=True)
-  results = main(content["data"], False)
+  results = process_image(content["data"])
   return jsonify(results=results)
 
 """
