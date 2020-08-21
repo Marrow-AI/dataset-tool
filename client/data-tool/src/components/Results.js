247df3d811381d8b295267514efec171fcf057a6
shirin <shirinanlen@gmail.com>
2020-08-21 17:42:02 -0400

    design

--- client/data-tool/src/components/Results.js
+++ client/data-tool/src/components/Results.js
@@ -1,82 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from 'react-redux';
-import store from '../state';
 
 export default function Results(props) {
   const keyword = useSelector(state => state.keyword);
-  const socket = useSelector(state => state.socket);
   const cropImages = useSelector(state => state.cropImages);
-  const images64 = useSelector(state => state.images64);
-
-  const corsServer = 'https://clump.systems/'; // avner change this to cors fetch in python! 
   let history = useHistory();
-  const [cleanImages, setCleanImages] = useState()
-  const [visiblebtn, setVisiblebtn] = useState(false)
+  const [showModel, setShowModel] = useState(false);
+  const [visible, setDisible] = useState(false)
 
   const goTrain = () => {
     console.log('train me!')
     history.push("/train")
   }
 
-
-  //   showLoading();
-  //   setTimeout(() => {
-  //     hideLoading()
-  //   }, 2000);
-  //   changeButtonText('Try Again?');
-  //   setvisibleTraining(true)
-
-  // }
-
-
-
-  async function fetchPoses () {
-    const {numOfPeople, numOfPermutations} = props.match.params;
-    console.log('fetching poses ', numOfPeople, numOfPermutations);
-
-    for( const singleImg of images64) {
-      const res = await fetch('http://52.206.213.41:22100/pose', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          data: singleImg,
-          numOfPeople,
-          numOfPermutations
-        })
-      })
-      if (res.ok) {
-        const data = await res.json();
-        for( let imageText of data.results) {
-          store.dispatch({
-            type: 'CROP_IMAGE',
-            cropImages: "data:image/jpg;base64," + imageText
-          })
-        } 
-      } else {
-        const text = await res.text();
-        throw new Error(text); 
-      }
-    }
-  }
-
-return (
-  <div className='secondScreen'>
-        <div className='leftSection'>
-          <h1 className='title result'>2.Results for:<span className='title result-before'>{keyword}</span> </h1>
-          {/* <p className='noImages'>No.images found:<span className="number"> {cleanImages.length + 1}</span></p><br /> */}
-          <div className='explaining'>
-            <p className='explain main'>What you just did was an example for <strong>Data Scraping</strong>.</p>
-            <p className='explain two'>Data Scraping is a technique in which a computer program extracts data usually from another program,<br /> <strong>in our case: Google Images</strong>.</p>
-            <p className='explain two'> This is a common techniqe when trying to collecting Machine Learning datasets.</p>
-            <p className='explain two'>Next step will be cleaning and orginaizing our data</p>
-            <button disabled={visiblebtn} className='more' onClick={goTrain}>let's train</button>
+  return (
+    <div className="mainTitle">
+      <h1 className='title result'>Final results for what was <span className='title result-before final'>{keyword}</span> </h1>
+      <div className='secondScreen'>
+        <div className='leftSection result'>
+          <div className='explaining result'>
+            <h2 className='explain main edit'>Step Four: Moment before training</h2>
+            <p className='explain two'>Now will be a stage where we will manully curate the images we received.<br />
+             Checking what kind of "human's bodies" the Neural Network sent back.</p>
+            <p className='explain two'> Another important aspect we should keep in mind
+             is the diversity of the dataset &mdas; is it represent all we need?  </p>
+            <p className='explain two'>If everything is Okay and we are pleased, we are proceeding with towward Training. </p>
+            <button disabled={visible} className='more' onClick={() => setShowModel(!showModel, setDisible(!visible))}>let's train</button>
           </div>
         </div>
 
+        {showModel &&
+          <div class="model">
+            <h2 className='explain main final'>MSG</h2>
+            <p className='explain modelText'>Training is a process that takes days if not weeks.</p>
+            <p className='explain modelText'> The training results you are about to see was done on "Happy Family dinner" dataset </p>
+            <p className='explain modelText'> It took 84 hours on ~6400 images that were collected more or less in this method. </p>
+            <button className='more modelText' onClick={goTrain}>I understand!</button>
+          </div>
+        }
+
         <div className='imageContainer'>
           <div className='images'>
-            {cropImages.map((cleanImages,index) => (
+            {cropImages.map((cleanImages, index) => (
               <div key={index}>
                 <img src={cleanImages} alt="" />
               </div>
@@ -84,6 +50,6 @@ return (
           </div>
         </div>
       </div>
-    
-)
+    </div>
+  )
 }
